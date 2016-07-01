3035fbab37bb913d1b9bc73973bddaa46753da0b
themikefuller <emailthemike@gmail.com>
2016-07-01 14:27:05 -0400

    Fix for CORS support

--- src/rapid.php
+++ src/rapid.php
@@ -186,6 +186,10 @@ class Rapid {
             $origin = $_SERVER['HTTP_ORIGIN'];
             header("Access-Control-Allow-Origin: $origin");
         }
+        if ($allowed[0] == '*') {
+            header("Access-Control-Allow-Origin: *");
+        }
+
         header("Access-Control-Allow-Methods: $methods");
     }
 
